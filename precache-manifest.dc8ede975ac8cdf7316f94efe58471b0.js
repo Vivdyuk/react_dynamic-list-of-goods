fef9647c9022ab0ccbe716ac3ce1eb524ab32367
Ruslan Vyvdyuk <84183046+Vivdyuk@users.noreply.github.com>
2021-08-02 18:14:45 +0300

    Updates

---
+++ precache-manifest.dc8ede975ac8cdf7316f94efe58471b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aec757db5bff55f722a825d7ed1fa0b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d8950d360ecb148259",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "4cad486b2f867f262c9e",
+    "url": "./static/js/2.aac25175.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.aac25175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d8950d360ecb148259",
+    "url": "./static/js/main.b4fae86f.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
